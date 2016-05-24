8ffb9accdc78071d5e9aa8fd4a7a3e803c985e04
microslayer <leffpb@gmail.com>
2016-05-23 23:30:32 -0400

    first commit

---
+++ app/src/main/java/com/example/pearlleff/mapapp4/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pearlleff.mapapp4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //Create the intent to start another activity
+                Intent intent = new Intent(view.getContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
